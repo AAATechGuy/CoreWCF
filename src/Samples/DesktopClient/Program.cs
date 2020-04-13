8ea932c669edf6c6be5ec0d403e547df8efc0fda
Abin Abraham Alichan <abinaa@microsoft.com>
2020-04-13 16:42:59 -0700

    try repro issue #2

--- src/Samples/DesktopClient/Program.cs
+++ src/Samples/DesktopClient/Program.cs
@@ -2,40 +2,81 @@
 using System.ServiceModel;
 using System.Text;
 
-namespace DesktopClient
+namespace NetCoreClient
 {
     class Program
     {
-        private readonly static string _basicHttpEndPointAddress = @"http://localhost:8080/basichttp";
-        private readonly static string _soapEnvelopeContent = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"><soapenv:Body><Echo xmlns = \"http://tempuri.org/\" ><text>Hello</text></Echo></soapenv:Body></soapenv:Envelope>";
+        private readonly static string _basicHttpEndPointAddress = @"http://localhost:3090/clientcenter/mt";
+        private readonly static string _soapEnvelopeContent = @"
+<s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/' _LoggingListEnvelopeCount='2'>
+  <s:Header _LoggingListHeaderCount='5'>
+    <h:RequestDetails xmlns:h='http://advertising.microsoft.com/2009/08/clientcenter/mt' xmlns:i='http://www.w3.org/2001/XMLSchema-instance' _LoggingListRequestDetailsCount='4'>
+      <h:ClientApplication>CampaignWebUI</h:ClientApplication>
+      <h:ClientMachine>RD00155DF9FDC1</h:ClientMachine>
+      <h:RequestId>f62dfba5-389b-495c-990a-d60bf3c9944a</h:RequestId>
+      <h:SessionId>54d694e5-9112-4686-ab71-ab9c0077d205</h:SessionId>
+    </h:RequestDetails>
+    <h:TrackingId xmlns:h='http://advertising.microsoft.com/2009/08/clientcenter/mt'>716ae182-b09b-4589-b587-5744991f70ae</h:TrackingId>
+    <h:UserToken xmlns:h='http://advertising.microsoft.com/2009/08/clientcenter/mt'>userToken</h:UserToken>
+  </s:Header>
+  <s:Body _LoggingListBodyCount='1'>
+    <CustomerFetchLightRequest xmlns='http://advertising.microsoft.com/2009/08/clientcenter/mt' _LoggingListCustomerFetchLightRequestCount='1'>
+      <Id>1</Id>
+      <IdWorking>2</IdWorking>
+    </CustomerFetchLightRequest>
+  </s:Body>
+</s:Envelope>".Replace("'", "\"");
 
         static void Main(string[] args)
         {
-            var factory = new ChannelFactory<Contract.IEchoService>(new NetTcpBinding(), new EndpointAddress("net.tcp://localhost:8808/nettcp"));
+            CallUsingWcf();
+            CallUsingWebRequest();
+
+            Console.WriteLine("Hit enter to exit");
+            Console.ReadLine();
+        }
+
+        private static void CallUsingWcf()
+        {
+            var factory = new ChannelFactory<Contract.IEchoService>(new BasicHttpBinding(), new EndpointAddress(_basicHttpEndPointAddress));
             factory.Open();
             var channel = factory.CreateChannel();
             ((IClientChannel)channel).Open();
-            Console.WriteLine("net.tcp Echo(\"Hello\") => " + channel.Echo("Hello"));
+
+            var result = channel.CustomerFetchLight(
+                new Contract.CustomerFetchLightRequest()
+                {
+                    Id = 1,
+                    IdWorking = 2
+                });
+
+            Console.WriteLine("\r\n==> httpWcfCall CustomerFetchLight.response => " + result.Name);
+            if (result.Name.Contains("1_2")) { Console.WriteLine("SUCCESS: valid response"); }
+            else { Console.WriteLine("FAIL: invalid response"); }
+
             ((IClientChannel)channel).Close();
             factory.Close();
 
-            factory = new ChannelFactory<Contract.IEchoService>(new BasicHttpBinding(), new EndpointAddress(_basicHttpEndPointAddress));
-            factory.Open();
-            channel = factory.CreateChannel();
-            ((IClientChannel)channel).Open();
-            Console.WriteLine("http Echo(\"Hello\") => " + channel.Echo("Hello"));
-            ((IClientChannel)channel).Close();
-            factory.Close();
+            //// factory = new ChannelFactory<Contract.IEchoService>(new NetTcpBinding(), new EndpointAddress("net.tcp://localhost:8808/nettcp"));
+            //// factory.Open();
+            //// channel = factory.CreateChannel();
+            //// ((IClientChannel)channel).Open();
+            //// Console.WriteLine("net.tcp Echo(\"Hello\") => " + channel.Echo("Hello"));
+            //// ((IClientChannel)channel).Close();
+            //// factory.Close();
 
-            // Complex type testing
-            factory = new ChannelFactory<Contract.IEchoService>(new BasicHttpBinding(), new EndpointAddress(_basicHttpEndPointAddress));
-            factory.Open();
-            channel = factory.CreateChannel();
-            ((IClientChannel)channel).Open();
-            Console.WriteLine("http EchoMessage(\"Complex Hello\") => " + channel.ComplexEcho(new Contract.EchoMessage() { Text = "Complex Hello" }));
-            ((IClientChannel)channel).Close();
-            factory.Close();
+            //// // Complex type testing
+            //// factory = new ChannelFactory<Contract.IEchoService>(new BasicHttpBinding(), new EndpointAddress(_basicHttpEndPointAddress));
+            //// factory.Open();
+            //// channel = factory.CreateChannel();
+            //// ((IClientChannel)channel).Open();
+            //// Console.WriteLine("http EchoMessage(\"Complex Hello\") => " + channel.ComplexEcho(new Contract.EchoMessage() { Text = "Complex Hello" }));
+            //// ((IClientChannel)channel).Close();
+            //// factory.Close();
+        }
 
+        private static void CallUsingWebRequest()
+        {
             // 
             // The following sample, creates a basic web request to the specified endpoint, sends the SOAP request and reads the response
             // 
@@ -46,8 +87,7 @@ namespace DesktopClient
 
             // Create the web request
             var webRequest = System.Net.WebRequest.Create(new Uri(_basicHttpEndPointAddress));
-            webRequest.Headers.Add("SOAPAction", "http://tempuri.org/IEchoService/Echo");
-
+            webRequest.Headers.Add("SOAPAction", "CustomerFetchLight");
             webRequest.ContentType = "text/xml";
             webRequest.Method = "POST";
             webRequest.ContentLength = bodyContentBytes.Length;
@@ -62,12 +102,12 @@ namespace DesktopClient
                 using (System.IO.StreamReader responsereader = new System.IO.StreamReader(responseStream))
                 {
                     var soapResponse = responsereader.ReadToEnd();
-                    Console.WriteLine($"Http SOAP Response: {soapResponse}");
+                    Console.WriteLine($"\r\n==> WebRequestHttp SOAP Response: {soapResponse}");
+
+                    if (soapResponse.Contains("1_2")) { Console.WriteLine("SUCCESS: valid response"); }
+                    else { Console.WriteLine("FAIL: invalid response"); }
                 }
             }
-
-            Console.WriteLine("Hit enter to exit");
-            Console.ReadLine();
         }
     }
 }
